80630b1441059d6dd45f35b985d18e3cf0fbccfa
Thisen <thisen-dk@hotmail.com>
2020-05-02 23:47:59 +0200

    WIP

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -2,3 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+
+const el: HTMLDivElement = document.createElement("div");
+el.innerText = "Hello, TypeScript";
+el.className = "ts1";
+document.body.appendChild(el);
